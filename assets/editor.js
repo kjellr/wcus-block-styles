f6e8119e29b296fea84d0c4a01681401c1394a09
Kjell Reigstad <kjell@kjellr.com>
2021-09-01 14:54:22 -0400

    Be even more aggressive about lettering.js

--- assets/editor.js
+++ assets/editor.js
@@ -1,10 +1,5 @@
 wp.domReady( function() {
-	const getSelectedBlock = () => wp.data.select( 'core/editor' ).getSelectedBlock();
-	let selectedBlock = getSelectedBlock();
 	wp.data.subscribe(() => {
-		const newSelectedBlock = getSelectedBlock();
-		if ( newSelectedBlock === null ) {
-			jQuery(".is-style-baseline-shift:not(.is-selected)").lettering();
-		}
+		jQuery(".is-style-baseline-shift:not(.is-selected)").lettering();
 	});
 });
